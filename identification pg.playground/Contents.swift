80f05c85a7a5716d2f812b3c05abf39a2890909d
Sbuzlouf <sumayaq8.06@gmail.com>
2022-08-03 18:33:06 +0300

    var & let been used

---
+++ identification pg.playground/Contents.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+
+var favouriteNumber = 15
+var name = "Sumaya"
+var hobby = "doing nothing"
+let BirthOfDate = 2006
+
+    print("Hello my name is \(name). \(favouriteNumber) is my favourite number. My hobby is \(hobby). \n In \(BirthOfDate) I was born.")
---
+++ identification/identification/identificationApp.swift
@@ -0,0 +1,17 @@
+//
+//  identificationApp.swift
+//  identification
+//
+//  Created by Sumaya Buzlouf on 03/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct identificationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
